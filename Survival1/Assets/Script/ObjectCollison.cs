d03bc28462bd757ed73265da0390ec1169b180ad
Yurimahendra <yurimahendra40@gmail.com>
2020-04-14 22:09:05 +0700

    pertemuan 4,5,6,7

---
+++ Survival1/Assets/Script/ObjectCollison.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectCollison : MonoBehaviour {
+
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.tag == "ObjectMusuh")
+		{
+			Debug.Log("Collide!");
+			AudioSource aud = gameObject.GetComponent<AudioSource>();
+			aud.Play();
+			col.gameObject.SetActive(false);
+		}
+	}
+}
